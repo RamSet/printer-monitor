91f03ddd6cbd0ee5d3721c563be5a844f545331b
RamSet <ramset@gmail.com>
2019-11-05 10:41:21 -0700

    Added Screen On/Off button code.

--- printermonitor/printermonitor.ino
+++ printermonitor/printermonitor.ino
@@ -52,6 +52,18 @@ SOFTWARE.
   SSD1306Wire     display(I2C_DISPLAY_ADDRESS, SDA_PIN, SCL_PIN); // this is the default
 #endif
 
+/*************Raz - Button***********/
+const int buttonPin = D3;
+
+int buttonState;             // the current reading from the input pin
+int lastButtonState = HIGH;   // the previous reading from the input pin
+
+// the following variables are unsigned long's because the time, measured in miliseconds,
+// will quickly become a bigger number than can be stored in an int.
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
+/*************Raz - Button***********/
+
 OLEDDisplayUi   ui( &display );
 
 void drawProgress(OLEDDisplay *display, int percentage, String label);
@@ -198,6 +210,10 @@ String COLOR_THEMES = "<option>red</option>"
                             
 
 void setup() {
+ 
+  /*************Raz - Button***********/
+  pinMode(buttonPin, INPUT);
+  /*************Raz - Button***********/
   Serial.begin(115200);
   SPIFFS.begin();
   delay(10);
@@ -361,7 +377,43 @@ void findMDNS() {
 // Main Looop
 //************************************************************
 void loop() {
-  
+ 
+   /*************Raz - Button***********/
+   // read the state of the switch into a local variable:
+  int reading = digitalRead(buttonPin);
+
+  // check to see if you just pressed the button
+  // (i.e. the input went from LOW to HIGH),  and you've waited
+  // long enough since the last press to ignore any noise:
+
+  // If the switch changed, due to noise or pressing:
+  if (reading != lastButtonState) {
+    // reset the debouncing timer
+    lastDebounceTime = millis();
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    // whatever the reading is at, it's been there for longer
+    // than the debounce delay, so take it as the actual current state:
+
+    // if the button state has changed:
+    if (reading != buttonState) {
+      buttonState = reading;
+
+      // only toggle the LED if the new button state is HIGH
+      if (buttonState == LOW) {
+        enableDisplay(true);
+      }
+       if (buttonState == HIGH) {
+        enableDisplay(false);
+      }
+    }
+  }
+
+  // save the reading.  Next time through the loop,
+  // it'll be the lastButtonState:
+  lastButtonState = reading;
+ /*************Raz - Button***********/
    //Get Time Update
   if((getMinutesFromLastRefresh() >= minutesBetweenDataRefresh) || lastEpoch == 0) {
     getUpdateTime();
